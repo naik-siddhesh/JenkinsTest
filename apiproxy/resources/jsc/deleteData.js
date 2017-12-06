66cb9a04619491fc323d11af0c306e50e4a4d262
naik-siddhesh <naik.siddhesh@tcs.com>
2017-12-06 13:21:05 +0530

    maven code

--- apiproxy/resources/jsc/deleteData.js
+++
@@ -1,5 +0,0 @@
-for(var i=0;i<50;i++)
-{var myRequest2 = new Request("https://baas-ug008mr.apigee.net/jnjdn/jnjdev/stores?access_token=YWMtRh1MGO0Eea0wOspJwCkfAAAAVo7bT9-IZe-WflXY02XA-mmAT8vtnm2Vvw&limit=226", 'DELETE');
-var exchange=httpClient.send(myRequest2);
-exchange.waitForComplete();
-}
